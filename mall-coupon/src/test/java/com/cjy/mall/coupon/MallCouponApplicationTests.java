595a32e479c35d97a3cee89a18aa378f5d4d821c
chenjy01 <chenjunyu102@126.com>
2020-08-20 00:40:06 +0800

    项目初始化

---
+++ mall-coupon/src/test/java/com/cjy/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjy.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/com/cjy/mall/ware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjy.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
